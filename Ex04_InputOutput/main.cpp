7a4f789d325c70e6acf3274a3f1d7ef7ed5e5224
Jaewon Lee <yesjaewon@gmail.com>
2023-08-27 20:28:28 +0900

    Ex04 clear

--- Ex04_InputOutput/main.cpp
+++ Ex04_InputOutput/main.cpp
@@ -20,7 +20,7 @@ int main()
         cout << "원하는 문장을 입력해주세요." << endl;
         cout << "입력: ";
 
-        // cin >> user_input;
+        // cin >> user_input; >>: extraction operator
 
         cin.getline(user_input, sizeof(user_input));
 
@@ -28,14 +28,20 @@ int main()
     */
 
     int number = -1;
-
-    cin >> user_input;
+    int number2 = 1;
+    
+    cout << "문자열을 입력하세요: " << endl;
+    cin >> user_input; // cin은 만약 입력에 빈칸이 있다면 거기까지만 input으로 받아온다.
+    // 만약 빈칸 상관없이 모든 입력은 받아오고 싶다면: cin.getline()
     // cin.getline(user_input, sizeof(user_input));
-    cin.ignore(100, '\n');
+    cin.ignore(100, '\n'); // 최대 100글자까지만 받겠다. \n이 있으면 그때부터 무시하겠다.
 
+    cout << "숫자를 순서대로 입력하세요:" << endl;
     cin >> number;
+    cout << "숫자를 순서대로 입력하세요:" << endl;
+    cin >> number2;
 
-    cout << user_input << " " << number << endl;
+    cout << user_input << " " << number+number2 << endl;
 
     // 참고: cin.ignore(numeric_limits<streamsize>::max(),'\n')
 
