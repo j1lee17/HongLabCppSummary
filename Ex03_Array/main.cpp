f38febb1f41cadde8d92f2742f8756e819502825
Jaewon Lee <yesjaewon@gmail.com>
2023-08-27 19:50:57 +0900

    Ex03 clear

--- Ex03_Array/main.cpp
+++ Ex03_Array/main.cpp
@@ -13,10 +13,10 @@ int main()
     int c = 3;
     // ...
 
-    // 같은 자료형의 데이터를 저장하기 위해 메모리를 미리 잡아놓은 것
+    // 배열(array): 같은 자료형의 데이터를 저장하기 위해 메모리를 미리 잡아놓은 것
     int my_array[3] = {1, 2, 3}; // 초기화할 때는 {} 안에 값 나열
 
-    // 인덱싱 (zero-based)
+    // 인덱싱 (zero-based): python 처럼 index는 0부터 시작.
     cout << my_array[0] << " "
          << my_array[1] << " "
          << my_array[2] << endl; // 추측해보세요
@@ -31,8 +31,9 @@ int main()
     // cout << my_array[10000] << endl;
 
     // 문자열은 기본적으로 문자의 배열
-    char name[] = "Hello, World!"; // 문자''와 문자열"" 구분
-    // Null character '\0'
+    char name[] = "Hello, World!"; // 문자''와 문자열"" 구분. '': 하나의 문자에 사용. "": 문자열에 사용.
+    // 초깃값 생략 가능. 문자열 배열의 크기가 알아서 할당됨.
+    // Null character '\0' 를 c/cpp에서는 마지막에 자동으로 추가를 한다. 따라서, 13 literal이 아니라 14 literal이 되는 것.
     cout << name << " " << sizeof(name) << endl; // 추측해보세요
 
     name[0] = 'A';
@@ -41,9 +42,8 @@ int main()
 
     cout << name << endl; // 추측해보세요
 
-    name[2] = '\0'; // 추측해보세요
-
-    cout << name << endl; // 추측해보세요
+    name[2] = '\0'; // 추측해보세요. 
+    cout << name << endl; // 추측해보세요. index 2이후로 아무 것도 출력하지 않게 됨. cout 내부에서는 index 순서대로 출력을 하다가 null character를 만난 것.
 
     return 0;
 }
